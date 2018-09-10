eab4f56eb50f772d6b0276bfe6febd879309c4ff
Felix Guo <felixguoxiuping@gmail.com>
2018-09-10 11:38:07 -0400

    roommates: added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    }
+};
