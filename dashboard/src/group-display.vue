60f6a2fdc7b290cffa86a1641db3957fb3cfa7ad
Felix Guo <felixguoxiuping@gmail.com>
2018-09-23 22:46:25 -0400

    roommates: added new Vue framework files
    This includes all the dashboard and configurations.

---
+++ dashboard/src/group-display.vue
@@ -0,0 +1,103 @@
+<template>
+    <div>
+        <div class="tab-bar mdc-theme--primary-bg">
+            <mdc-tab-bar class="mdc-theme--primary-bg">
+                <mdc-tab
+                    class="tab-item"
+                    v-for="tab in tabs"
+                    v-on:click="selectedTab = tab"
+                    v-bind:key="tab[0]"
+                    v-bind:active="selectedTab && selectedTab[0] == tab[0]">
+                        {{ tab[0] }}
+                </mdc-tab>
+            </mdc-tab-bar>
+        </div>
+        <div class="content">
+            <component
+                v-if="fullGroup !== undefined && selectedTab !== undefined && selectedTab[1] !== undefined"
+                v-bind:is="selectedTab[1]"
+                v-bind:group="fullGroup"
+                v-bind:root="root"
+                v-bind:reloadGroup="reloadGroup" />
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+import manageGroupTab from './tabs/manage-group';
+
+export default {
+    name: 'group-display',
+    props: {
+        root: Object,
+        group: Object
+    },
+    data() {
+        return {
+            selectedTab: undefined,
+            tabs: [
+                ["Overview", undefined],
+                ["Expenses", undefined],
+                ["Groceries", undefined],
+                ["Chores", undefined],
+                ["Manage Group", manageGroupTab],
+            ],
+            fullGroup: undefined
+        }
+    },
+    mounted() {
+        this.resetTabs();
+        this.reloadGroup();
+    },
+    watch: {
+        group: function() {
+            this.resetTabs();
+            this.reloadGroup();
+        }
+    },
+    methods: {
+        resetTabs() {
+            if (this.tabs.length > 0) {
+                this.selectedTab = this.tabs[0];
+            } else {
+                this.selectedTab = undefined;
+            }
+        },
+        reloadGroup() {
+            axios.get(`/api/staging/group/${this.group.id}`).then(response => {
+                this.fullGroup = response.data;
+            }).catch(error => {
+                this.root.showRequestError(error);
+            });
+        }
+    },
+    components: {
+        manageGroupTab
+    },
+};
+</script>
+
+<style>
+.tab-bar {
+    width: 100%;
+    text-align: left;
+}
+
+.tab-bar nav {
+    margin: 0px!important;
+}
+
+.tab-item {
+    color: #FFF!important;
+}
+
+.mdc-tab-bar__indicator {
+    background-color: #FFF!important;
+}
+
+.content {
+  padding: 24px 48px;
+}
+</style>
