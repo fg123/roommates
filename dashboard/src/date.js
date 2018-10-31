ac572d2c04ef81995a144ebab7b2d3e90b53f5a5
robertpan226 <robertpan226@gmail.com>
2018-10-31 18:25:23 -0400

    toDateString now throws exception if UTC is undefined.

--- dashboard/src/date.js
+++ dashboard/src/date.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 
 export default {
     toDateString: function(utc) {
+        if (utc === undefined) throw 'Date provided is not defined!';
         return moment.utc(utc).local().format('MMMM DD, YYYY');
     },
     toMonthYearString: function(utc) {
