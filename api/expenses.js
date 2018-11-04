fe69ce4d7a0be0687ffee8553c1c498fa1e53617
robertpan226 <robertpan226@gmail.com>
2018-11-03 23:53:21 -0400

    Refactored previous commit.

--- api/expenses.js
+++ api/expenses.js
@@ -150,7 +150,7 @@ router.post('/group/:groupId/expenses/:expenseGroupId/transactions/add', functio
         owingsDelta[owee] = 0; /* making sure owee is also set to 0 in case that owee not in owers */
 
         /* Value is a string in dollars and stored as cents in database */
-        owingsDelta[owee] -= Number(value) * 100;
+        owingsDelta[owee] = -(Number(value) * 100);
         
         /* Split amount amongst rest */
         const totalCents = Math.floor(Number(value) * 100);
